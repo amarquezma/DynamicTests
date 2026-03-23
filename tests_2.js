fcf02258ec257ac66eda2c65b3c5381309f748f3
alvaro marquez <alvaromarquezmr@gmail.com>
2026-03-23 19:28:05 +0100

    tests

---
+++ tests_2.js
@@ -0,0 +1,15 @@
+const TEST_DATA = {
+  subjects: [
+    {
+      id: "lgm",
+      name: "LGM",
+      units: [
+        {
+          id: "tema1",
+          name: "1 Questions from tema1",
+          tests: [{}],
+        },
+      ],
+    },
+  ],
+};
